432fe65a2155f25ef23b8e0c5a863c68eb962ecf
Leo <lee2928030@naver.com>
2021-08-01 20:22:38 +0900

    Migrate to Androidx

--- sample/src/main/java/com/watermark/androidwm/sample/MainActivity.java
+++ sample/src/main/java/com/watermark/androidwm/sample/MainActivity.java
@@ -20,7 +20,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
